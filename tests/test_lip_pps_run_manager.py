4f6ba90cfd3d2fc2f15c132f35528754c3ccd758
Cristóvão da Cruz e Silva <CrisXed@gmail.com>
2022-09-27 17:51:00 +0100

    Added tests for basic run manager functionality

--- tests/test_lip_pps_run_manager.py
+++ tests/test_lip_pps_run_manager.py
@@ -1,6 +1,20 @@
+from pathlib import Path
 
+import lip_pps_run_manager as RM
 from lip_pps_run_manager.cli import main
 
 
 def test_main():
     main([])
+
+
+def test_run_manager():
+    John = RM.RunManager(Path("."))
+    assert John.path_storage == Path(".")
+
+
+def test_fail_run_manager():
+    try:
+        RM.RunManager(".")
+    except TypeError as e:
+        assert str(e) == ("The `path_to_run_storage` must be a Path type object, received object of type <class 'str'>")
