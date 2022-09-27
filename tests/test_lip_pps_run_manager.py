178c7209b84051fc5d7926be3f47c01c6bfd04c6
Cristóvão da Cruz e Silva <CrisXed@gmail.com>
2022-09-27 18:59:22 +0100

    Added tests for function checking for run existance

--- tests/test_lip_pps_run_manager.py
+++ tests/test_lip_pps_run_manager.py
@@ -1,3 +1,4 @@
+import shutil
 from pathlib import Path
 
 import lip_pps_run_manager as RM
@@ -19,3 +20,25 @@ def test_fail_run_manager():
         RM.RunManager(".")
     except TypeError as e:
         assert str(e) == ("The `path_to_run_directory` must be a Path type object, received object of type <class 'str'>")
+
+
+def test_run_exists_function():
+    runName = "hopefully_unique_run_name"
+    p = Path(".") / runName
+    p.mkdir(exist_ok=True)
+    (p / 'run_info.txt').touch()
+    assert RM.run_exists(Path("."), runName)  # Test when exists
+    shutil.rmtree(p)
+    assert not RM.run_exists(Path("."), runName)  # Test when doesn't exist
+
+
+def test_fail_run_exists_function():
+    try:
+        RM.run_exists(".", "a")
+    except TypeError as e:
+        assert str(e) == ("The `path_to_directory` must be a Path type object, received object of type <class 'str'>")
+
+    try:
+        RM.run_exists(Path("."), 2)
+    except TypeError as e:
+        assert str(e) == ("The `run_name` must be a str type object, received object of type <class 'int'>")
