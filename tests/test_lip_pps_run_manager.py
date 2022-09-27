1121a53e7cf56f618b28b1b1a06d0339de05ef3c
Cristóvão da Cruz e Silva <CrisXed@gmail.com>
2022-09-27 21:34:01 +0100

    Added additional tests for ancillary functions

--- tests/test_lip_pps_run_manager.py
+++ tests/test_lip_pps_run_manager.py
@@ -42,3 +42,51 @@ def test_fail_run_exists_function():
         RM.run_exists(Path("."), 2)
     except TypeError as e:
         assert str(e) == ("The `run_name` must be a str type object, received object of type <class 'int'>")
+
+
+def test_clean_path_function():
+    assert isinstance(RM.clean_path(Path(".")), Path)  # Test return type
+    # TODO: assert RM.clean_path(Path("./Run@2")) == Path("./Run2") # Test actual cleaning
+
+
+def test_fail_clean_path_function():
+    try:
+        RM.clean_path(".")
+    except TypeError as e:
+        assert str(e) == ("The `path_to_clean` must be a Path type object, received object of type <class 'str'>")
+
+
+def test_create_run_function():
+    basePath = Path("/tmp")
+    runName = "testRun_21"
+
+    assert RM.create_run(basePath, runName) == basePath / runName
+    assert (basePath / runName).is_dir()
+    assert (basePath / runName / "run_info.txt").is_file()
+    # TODO: Check contents of run_info.txt file
+    shutil.rmtree(basePath / runName)
+    assert not (basePath / runName).is_dir()
+
+
+def test_fail_create_run_function():
+    basePath = Path("/tmp")
+    runName = "testRun_21"
+
+    try:
+        RM.create_run(".", runName)
+    except TypeError as e:
+        assert str(e) == ("The `path_to_directory` must be a Path type object, received object of type <class 'str'>")
+
+    try:
+        RM.create_run(basePath, 2)
+    except TypeError as e:
+        assert str(e) == ("The `run_name` must be a str type object, received object of type <class 'int'>")
+
+    try:
+        RM.create_run(basePath, runName)
+        RM.create_run(basePath, runName)
+    except RuntimeError as e:
+        shutil.rmtree(basePath / runName)
+        assert str(e) == (
+            "Unable to create the run '{}' in '{}' because a directory with that name already exists.".format(runName, str(basePath))
+        )
