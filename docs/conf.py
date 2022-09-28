0c39eebfa12c2e1a86fd1c367f33257aa228f33f
Cristóvão da Cruz e Silva <CrisXed@gmail.com>
2022-09-28 20:22:22 +0100

    Bump version: 0.0.0 → 0.1.0

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ project = 'LIP_PPS_Run_Manager'
 year = '2022'
 author = 'Cristóvão B. da Cruz e Silva'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.0.0'
+version = release = '0.1.0'
 
 pygments_style = 'trac'
 templates_path = ['.']
