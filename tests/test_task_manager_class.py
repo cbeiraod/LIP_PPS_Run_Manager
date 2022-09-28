5c73eebb95cff070b334b80b5da05a50f792cbd1
Cristóvão da Cruz e Silva <CrisXed@gmail.com>
2022-09-28 17:15:38 +0100

    Added test for 'with' syntax and added a further test on the backup script

--- tests/test_task_manager_class.py
+++ tests/test_task_manager_class.py
@@ -1,5 +1,6 @@
 import shutil
 import tempfile
+import traceback
 from pathlib import Path
 
 import lip_pps_run_manager as RM
@@ -70,6 +71,13 @@ def test_fail_task_manager():
         except RuntimeError as e:
             assert str(e) == ("The 'path_to_run' ({}) does not look like the directory of a run...".format(runPath))
 
+    with PrepareRunDir() as handler:
+        runPath = handler.run_path
+        try:
+            RM.TaskManager(runPath, "myTask", drop_old_data=True, script_to_backup=runPath)
+        except RuntimeError as e:
+            assert str(e) == ("The 'script_to_backup', if set, must point to a file. It points to: {}".format(runPath))
+
 
 def test_task_manager_clean_task_directory():
     with PrepareRunDir() as handler:
@@ -86,3 +94,53 @@ def test_task_manager_clean_task_directory():
         assert not (John.task_path / "testFile.tmp").is_file()
         assert not (John.task_path / "testDir").is_dir()
         assert next(John.task_path.iterdir(), None) is None
+
+
+def test_task_manager_with():
+    with PrepareRunDir() as handler:
+        runPath = handler.run_path
+        John = RM.TaskManager(runPath, "myTask", drop_old_data=True, script_to_backup=None)
+
+        with John as john:
+            (john.task_path / "task_file.txt").touch()
+        assert (John.task_path / "task_report.txt").is_file()  # Test the __exit__ is correctly creating the report file
+
+        with open(John.task_path / "task_report.txt") as report_file:
+            first_line = report_file.readline()
+            assert first_line == "Task completed successfully with no errors\n"  # Test that the content of the report file are correct
+
+        try:
+            with John as john:  # Test __enter__ not allowing to reuse a TaskManager
+                2  # pragma: no cover
+        except RuntimeError as e:
+            assert str(e) == ("Once a task has processed its data, it can not be processed again. Use a new task")
+
+        John2 = RM.TaskManager(runPath, "myTask", drop_old_data=False, script_to_backup=None)
+        with John2 as john:
+            assert (john.task_path / "task_file.txt").is_file()  # Test __enter__ does not delete old files if not asked to
+
+        try:
+            John3 = RM.TaskManager(runPath, "myTask", drop_old_data=True, script_to_backup=Path(traceback.extract_stack()[-1].filename))
+            with John3 as john:
+                assert not (john.task_path / "task_file.txt").is_file()  # Test __enter__ does delete old files when asked to
+                raise RuntimeError(
+                    "This is to test the TaskManager error handling"
+                )  # Create an artificial error to trigger the report file to have error content in it
+        except RuntimeError as e:
+            assert str(e) == "This is to test the TaskManager error handling"
+        with open(John3.task_path / "task_report.txt") as report_file:
+            first_line = report_file.readline()
+            assert (
+                first_line == "Task could not be completed because there were errors\n"
+            )  # Test that the content of the report file are correct
+
+        assert (
+            John3.task_path / "backup.{}".format(Path(traceback.extract_stack()[-1].filename).parts[-1])
+        ).is_file()  # Test script backing up
+
+        try:
+            John4 = RM.TaskManager(runPath, "myTask", drop_old_data=True, script_to_backup=Path(traceback.extract_stack()[-1].filename))
+            with John4 as john:
+                John4._script_to_backup = runPath
+        except RuntimeError as e:
+            assert str(e) == "Somehow you are trying to backup a file that does not exist"
