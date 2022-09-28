bbce99dd249662eecbb36d016c244b8f234a159e
Cristóvão da Cruz e Silva <CrisXed@gmail.com>
2022-09-28 10:39:03 +0100

    updated tests to account for new __init__

--- tests/test_lip_pps_run_manager.py
+++ tests/test_lip_pps_run_manager.py
@@ -2,6 +2,7 @@ import shutil
 from pathlib import Path
 
 import lip_pps_run_manager as RM
+import lip_pps_run_manager.run_manager as internalRM
 from lip_pps_run_manager.cli import main
 
 
@@ -80,31 +81,31 @@ def test_run_exists_function():
     p = Path("/tmp") / runName
     p.mkdir(exist_ok=True)
     (p / 'run_info.txt').touch()
-    assert RM.run_exists(Path("/tmp"), runName)  # Test when exists
+    assert internalRM.run_exists(Path("/tmp"), runName)  # Test when exists
     shutil.rmtree(p)
-    assert not RM.run_exists(Path("/tmp"), runName)  # Test when doesn't exist
+    assert not internalRM.run_exists(Path("/tmp"), runName)  # Test when doesn't exist
 
 
 def test_fail_run_exists_function():
     try:
-        RM.run_exists(".", "a")
+        internalRM.run_exists(".", "a")
     except TypeError as e:
         assert str(e) == ("The `path_to_directory` must be a Path type object, received object of type <class 'str'>")
 
     try:
-        RM.run_exists(Path("."), 2)
+        internalRM.run_exists(Path("."), 2)
     except TypeError as e:
         assert str(e) == ("The `run_name` must be a str type object, received object of type <class 'int'>")
 
 
 def test_clean_path_function():
-    assert isinstance(RM.clean_path(Path(".")), Path)  # Test return type
+    assert isinstance(internalRM.clean_path(Path(".")), Path)  # Test return type
     # TODO: assert RM.clean_path(Path("./Run@2")) == Path("./Run2") # Test actual cleaning
 
 
 def test_fail_clean_path_function():
     try:
-        RM.clean_path(".")
+        internalRM.clean_path(".")
     except TypeError as e:
         assert str(e) == ("The `path_to_clean` must be a Path type object, received object of type <class 'str'>")
 
@@ -113,7 +114,7 @@ def test_create_run_function():
     basePath = Path("/tmp")
     runName = "testRun_21"
 
-    assert RM.create_run(basePath, runName) == basePath / runName
+    assert internalRM.create_run(basePath, runName) == basePath / runName
     assert (basePath / runName).is_dir()
     assert (basePath / runName / "run_info.txt").is_file()
     # TODO: Check contents of run_info.txt file
@@ -126,18 +127,18 @@ def test_fail_create_run_function():
     runName = "testRun_21"
 
     try:
-        RM.create_run(".", runName)
+        internalRM.create_run(".", runName)
     except TypeError as e:
         assert str(e) == ("The `path_to_directory` must be a Path type object, received object of type <class 'str'>")
 
     try:
-        RM.create_run(basePath, 2)
+        internalRM.create_run(basePath, 2)
     except TypeError as e:
         assert str(e) == ("The `run_name` must be a str type object, received object of type <class 'int'>")
 
     try:
-        RM.create_run(basePath, runName)
-        RM.create_run(basePath, runName)
+        internalRM.create_run(basePath, runName)
+        internalRM.create_run(basePath, runName)
     except RuntimeError as e:
         shutil.rmtree(basePath / runName)
         assert str(e) == (
