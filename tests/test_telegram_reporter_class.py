87cd40944986a2b3815f2a0232363a5c60eb5529
Cristóvão da Cruz e Silva <CrisXed@gmail.com>
2022-09-29 14:50:58 +0100

    Added tests for fails conditions of constructor

--- tests/test_telegram_reporter_class.py
+++ tests/test_telegram_reporter_class.py
@@ -11,3 +11,15 @@ def test_telegram_reporter():
 
     assert reporter.bot_token == "bot_token"
     assert reporter.chat_id == "chat_id"
+
+
+def test_fail_telegram_reporter():
+    try:
+        RM.TelegramReporter(1, "chat_id")
+    except TypeError as e:
+        assert str(e) == ("The `bot_token` must be a str type object, received object of type <class 'int'>")
+
+    try:
+        RM.TelegramReporter("bot_token", 1)
+    except TypeError as e:
+        assert str(e) == ("The `chat_id` must be a str type object, received object of type <class 'int'>")
