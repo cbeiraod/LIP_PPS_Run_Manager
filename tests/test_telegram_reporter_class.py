dcfccc768a75289ff2ed92d006eed87e4448ba0d
Cristóvão da Cruz e Silva <CrisXed@gmail.com>
2022-09-29 20:09:44 +0100

    Added tests for editing telegram messages

--- tests/test_telegram_reporter_class.py
+++ tests/test_telegram_reporter_class.py
@@ -10,17 +10,32 @@ class SessionReplacement:
         pass
 
     def get(self, url: str, data=None, timeout=None):
-        if self._error_type is not None:
-            if self._error_type == "KeyboardInterrupt":
-                raise KeyboardInterrupt
-            elif self._error_type == "Exception":  # pragma: no cover
-                raise Exception()
         self._params = {}
 
         self._params["url"] = url
         self._params["data"] = data
         self._params["timeout"] = timeout
+
+        if self._error_type is not None:
+            if self._error_type == "KeyboardInterrupt":
+                raise KeyboardInterrupt
+            elif self._error_type == "Exception":  # pragma: no cover
+                raise Exception()
+
         return self
+
+    def post(self, url: str, data=None, timeout=None):
+        self._params = {}
+
+        self._params["url"] = url
+        self._params["data"] = data
+        self._params["timeout"] = timeout
+
+        if self._error_type is not None:
+            if self._error_type == "KeyboardInterrupt":
+                raise KeyboardInterrupt
+            elif self._error_type == "Exception":  # pragma: no cover
+                raise Exception()
 
     def json(self):
         return self._params
@@ -97,5 +112,57 @@ def test_fail_telegram_reporter_send_message():
     reporter._session = sessionHandler2
     try:
         reporter.send_message(message_text=message, reply_to_message_id=None)
+    except RuntimeWarning as e:
+        assert str(e) == "Failed sending to telegram. Reason: Exception()"
+
+
+def test_telegram_reporter_edit_message():
+    sessionHandler = SessionReplacement()
+    bot_token = "bot_token"
+    chat_id = "chat_id"
+    reporter = RM.TelegramReporter(bot_token, chat_id)
+    reporter._session = sessionHandler  # This prevents actual HTTP requests from being generated
+
+    message = "Hello there"
+    message_id = "message_id"
+    reporter.edit_message(message_text=message, message_id=message_id)
+
+    retVal = sessionHandler.json()
+    assert retVal["timeout"] == 1
+    assert retVal["url"] == "https://api.telegram.org/bot{}/editMessageText".format(bot_token)
+    assert retVal["data"]["message_id"] == message_id
+    assert retVal["data"]['chat_id'] == chat_id
+    assert retVal["data"]['text'] == message
+
+
+def test_fail_telegram_reporter_edit_message():
+    sessionHandler = SessionReplacement(error_type="KeyboardInterrupt")
+    sessionHandler2 = SessionReplacement(error_type="Exception")
+    bot_token = "bot_token"
+    chat_id = "chat_id"
+    reporter = RM.TelegramReporter(bot_token, chat_id)
+    reporter._session = sessionHandler  # This prevents actual HTTP requests from being generated
+
+    message = "Hello there"
+    message_id = "message_id"
+
+    try:
+        reporter.edit_message(message_text=1, message_id=message_id)
+    except TypeError as e:
+        assert str(e) == ("The `message_text` must be a str type object, received object of type <class 'int'>")
+
+    try:
+        reporter.edit_message(message_text=message, message_id=1)
+    except TypeError as e:
+        assert str(e) == ("The `message_id` must be a str type object, received object of type <class 'int'>")
+
+    try:
+        reporter.edit_message(message_text=message, message_id=message_id)
+    except KeyboardInterrupt as e:
+        assert isinstance(e, KeyboardInterrupt)
+
+    reporter._session = sessionHandler2
+    try:
+        reporter.edit_message(message_text=message, message_id=message_id)
     except RuntimeWarning as e:
         assert str(e) == "Failed sending to telegram. Reason: Exception()"
