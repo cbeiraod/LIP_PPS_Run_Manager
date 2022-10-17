5ff49db1da4da7b4df7ec657fe65a4f5801062ea
Cristóvão da Cruz e Silva <CrisXed@gmail.com>
2022-10-17 15:07:17 +0100

    Added a rate limiter to respect the telegram rate limits

--- src/lip_pps_run_manager/telegram_reporter.py
+++ src/lip_pps_run_manager/telegram_reporter.py
@@ -5,6 +5,8 @@ Contains classes and functions used to manage bots to report things to telegram.
 
 """
 
+import datetime
+import time
 import warnings
 
 import requests
@@ -46,20 +48,28 @@ class TelegramReporter:
     _chat_id = None
     _session = None
 
-    def __init__(self, bot_token: str, chat_id: str):
+    _last_message_time = datetime.datetime.now() - datetime.timedelta(seconds=5)
+    _rate_limit = True  # If set, messages will be delayed to respect the rate limits set by telegram
+    _rate_min_time = datetime.timedelta(seconds=1)  # Minimum allowed time between messages
+
+    def __init__(self, bot_token: str, chat_id: str, rate_limit: bool = True):
         if not isinstance(bot_token, str):
             raise TypeError("The `bot_token` must be a str type object, received object of type {}".format(type(bot_token)))
 
         if not isinstance(chat_id, str):
             raise TypeError("The `chat_id` must be a str type object, received object of type {}".format(type(chat_id)))
 
+        if not isinstance(rate_limit, bool):
+            raise TypeError("The `rate_limit` must be a bool type object, received object of type {}".format(type(chat_id)))
+
         self._bot_token = bot_token
         self._chat_id = chat_id
         self._session = requests.Session()
+        self._rate_limit = rate_limit
 
     def __repr__(self):
         """Get the python representation of this class"""
-        return "TelegramReporter({}, {})".format(repr(self.bot_token), repr(self.chat_id))
+        return "TelegramReporter({}, {}, rate_limit={})".format(repr(self.bot_token), repr(self.chat_id), repr(self._rate_limit))
 
     @property
     def bot_token(self) -> str:
@@ -92,11 +102,16 @@ class TelegramReporter:
         if reply_to_message_id is not None:
             message_params["reply_to_message_id"] = reply_to_message_id
 
+        if self._rate_limit:
+            if datetime.datetime.now() - self._last_message_time < self._rate_min_time:
+                time.sleep((self._rate_min_time - (datetime.datetime.now() - self._last_message_time)).total_seconds())
+
         response = self._session.get(
             "https://api.telegram.org/bot{}/sendMessage".format(self.bot_token),
             data=message_params,
             timeout=1,
         )
+        self._last_message_time = datetime.datetime.now()
         return response.json()
 
     def send_message(self, message_text: str, reply_to_message_id: str = None):
@@ -155,6 +170,10 @@ class TelegramReporter:
             The ID of the message to edit
 
         """
+        if self._rate_limit:
+            if datetime.datetime.now() - self._last_message_time < self._rate_min_time:
+                time.sleep((self._rate_min_time - (datetime.datetime.now() - self._last_message_time)).total_seconds())
+
         response = self._session.post(
             "https://api.telegram.org/bot{}/editMessageText".format(self.bot_token),
             data={
@@ -165,6 +184,7 @@ class TelegramReporter:
             timeout=1,
         )
 
+        self._last_message_time = datetime.datetime.now()
         return response.json()
 
     def edit_message(self, message_text: str, message_id: str):
